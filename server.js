48be40d1168fc3d8438b0735fb3ba1fab2265ccb
Brett Cimbalik <bcimbali@gmail.com>
2018-06-08 21:13:03 -0500

    took sequelize out

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.listen(PORT, function() {
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
